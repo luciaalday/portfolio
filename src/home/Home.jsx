4192aed5cf4191b54cc0c54b181013c0f74e2cf1
Lucia Alday <123031285+luciaalday@users.noreply.github.com>
2026-01-03 19:07:01 -0700

    min changes

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,12 @@
 import './Home.css';
+import Monogram from '../img/monogram.png';
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <div className="home-nav">
-        <img src='../img/monogram.png' width='20px' alt='LA' />
+        <img src={Monogram} width='80px' alt='LA' />
         <Link className="home-nav-link">Home</Link>
       </div>
     </div>
