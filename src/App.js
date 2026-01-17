df9f362f72ad90c42afadd7d120ab61c03574984
Lucia Alday <123031285+luciaalday@users.noreply.github.com>
2026-01-16 20:43:03 -0700

    navbar updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './home/Home';
 
+import './assets/defaults.css';
+import './assets/home.css';
+import './assets/nav.css';
+
 function App() {
   return (
     <div className="App">
