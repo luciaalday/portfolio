daa70224a96691238a03b2d8f98d0414a5aabefb
Lucia Alday <123031285+luciaalday@users.noreply.github.com>
2026-02-24 13:45:38 -0700

    fix links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 /** src/pages/Homes.jsx */
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <main>
@@ -9,15 +11,19 @@ export default function Home() {
         </section>
         <section className="right">
           <p>I am...</p>
-          <a href="/Developer"><h2 className="item">A developer</h2></a>
-          <a href="/Researcher"><h2 className="item">A researcher</h2></a>
-          <a href="/Designer"><h2 className="item">A designer</h2></a>
-          <a href="/Resume"><h5 className="item">View my experience</h5></a>
+          <Link to="/Developer"><h2 className="item">A developer</h2></Link>
+          <Link to="/Researcher"><h2 className="item">A researcher</h2></Link>
+          <Link to="/Designer"><h2 className="item">A designer</h2></Link>
+          <Link to="/Resume"><h5 className="item">View my experience</h5></Link>
         </section>
       </article>
         <hr></hr>
       <article>
         <h2>About Me</h2>
+      </article>
+      <article>
+        <p>I am graduating from the University of Arizona with a B.S. in Computer Science this May of 2026.</p>
+        <p></p>
       </article>
     </main>
   );
